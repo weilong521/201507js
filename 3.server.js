c3d23123938616b571a706507e40200160550e8e
zfpx <zfpx@126.com>
2015-12-08 15:33:59 +0800

    请求和响应

---
+++ 3.server.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var menus = ["鱼香茄子","红烧茄子","酱香茄子"];
+/**
+ * @param req 请求
+ * @param res 响应
+ */
+function serve(req,res){
+  console.log(req.url);//请求的URL
+  console.log(req.headers);//请求头
+  console.log(req.method);
+  console.log(req.httpVersion);
+  var url = req.url;
+  if(url == '/'){
+    res.setHeader('Content-Type','text/html;charset=utf-8');
+    res.setHeader('zfname','zfpx');
+    res.write('<ul>');
+    menus.forEach(function(menu){
+      res.write('<li><a href="/'+menu+'">'+menu+'</a></li>');
+    });
+    res.write('</ul>');
+    res.end();//结束响应
+  }else{
+    res.setHeader('Content-Type','text/html;charset=utf-8');
+    res.write(decodeURIComponent(url.slice(1)));
+    res.end();//结束响应
+  }
+
+}
+
+//有客人进来的时候,调用serve函数进行处理
+var server = http.createServer(serve);
+server.listen(8080,'localhost');//让别人能找到你
+
+
+
